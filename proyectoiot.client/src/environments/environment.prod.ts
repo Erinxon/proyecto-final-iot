cb2005209a4d5377746904393e0835737f09cb03
Erinxon <30541728+Erinxon@users.noreply.github.com>
2022-04-21 19:52:14 -0400

    Update environment production

--- proyectoiot.client/src/environments/environment.prod.ts
+++ proyectoiot.client/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  MQTT_SERVICE_OPTIONS: {
+    host: '6436817f2603402d9cb925eb1359e52a.s2.eu.hivemq.cloud',
+    port: 8884,
+    protocol: 'wss',
+    username: 'Equipo1IoT',
+    connectTimeout: 4000,
+    password: 'Equipo1iot',
+    clean:true
+  },
+  connectUrl: 'wss://6436817f2603402d9cb925eb1359e52a.s2.eu.hivemq.cloud:8884/mqtt'
 };
